b0600e12e78b511384f9cb454779731ab4097a30
km-kubo <122439182+km-kubo@users.noreply.github.com>
2023-03-15 22:18:53 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   css: ["animate.css"],
   runtimeConfig: {
-    region: "ap-northeast-1",
+    region: "us-east-2",
     bucketName: "km-sd01-pokemon",
     public: {
       backendOrigin: "http://localhost:3000",
